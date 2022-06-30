f5c5c9acb672b404b25590f6ab363a4242d87fdf
ugbss <sani_abel@yahoo.com>
2022-06-30 15:37:46 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,13 @@
 
 /**
  * main - prints exactly a phrase 
- * - Dora Korpar, 
- *   followed by a new line 
- *   Return: 1 is successful
+ * Dora Korpar, 
+ * followed by a new line 
+ * Return: 1 is successful
  */
 int main(void)
 {
-char var[] = "and that piece of art is useeful\" - Dora Korpar, 2015-10-19\n";
+char var[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, var, sizeof(var) - 1);
 return (1);
 }
