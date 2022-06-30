830623879537682f00ea26ad6f231b9d8ac7ca06
ugbss <sani_abel@yahoo.com>
2022-06-30 15:28:36 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints exactly a phrase 
+ * - Dora Korpar, 
+ *   followed by a new line 
+ *   Return: always 0
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1)
+}`
