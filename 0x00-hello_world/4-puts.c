4df8f45b95e18e6bff3d21b43742b140bfb1d276
ugbss <sani_abel@yahoo.com>
2022-06-30 12:01:13 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #!/bin/bash 
 #include <stdio.h>
-int main()		
+
+/**
+ * main-main block 
+ * return:0
+ */
+int main(void)		
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
