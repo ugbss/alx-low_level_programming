aca91f64d4ca77cac7abb516e191ff9ac90d8773
ugbss <sani_abel@yahoo.com>
2022-07-01 18:18:21 -0700

    C programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, folowed by a new line
+ * Return: ALways 0 (SUccess)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+printsf("%d", n);
+printsf("\n");
+return (0);
+}
